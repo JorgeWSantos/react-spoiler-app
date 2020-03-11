ea3180a59aeda23649b15563ff03d25e8614da3d
Jorge <jorgeewillian00@hotmail.com>
2020-03-11 07:55:48 -0300

    alterações

---
+++ build/precache-manifest.222df8ba5249f5a6e8a60fe4c744ccd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8558114ea8bdc273b63c0d51fcc975",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "d81ee4e26f5f77cc5034",
+    "url": "/react/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "39375ab6a4933d7a80bf",
+    "url": "/react/static/css/main.92af5ff7.chunk.css"
+  },
+  {
+    "revision": "d81ee4e26f5f77cc5034",
+    "url": "/react/static/js/2.b2b385d5.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react/static/js/2.b2b385d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39375ab6a4933d7a80bf",
+    "url": "/react/static/js/main.b60d31e2.chunk.js"
+  },
+  {
+    "revision": "14dfd05395b6c3d7ee71",
+    "url": "/react/static/js/runtime-main.6167e587.js"
+  }
+]);
